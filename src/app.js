2fa8500ae2103e0e933b27d01b0da607599eb105
nikogagua <gaguanick2008@gmail.com>
2026-07-14 22:47:59 +0400

    Add JWT auth: register, login, authenticate and adminOnly middleware

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const authRoutes = require("./routes/auth");
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Loop Backend is running 🚀");
